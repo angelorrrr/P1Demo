c7895ee77f6332c6d68acd13f4d0aa0de0ab0609
Camila Luiza <camilaluiza1d@gmail.com>
2025-10-09 16:01:31 -0300

    Ajusta Main e DFS para resolver o Tema A

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,50 @@
-import org.example.algorythms.Kosaraju;
+package org.example;
+
+import org.example.algorythms.DFS;
 import org.example.graph.interfaces.Graph;
 import org.example.util.GraphView;
-import org.example.util.Menu;
 import org.example.util.TXTReader;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() throws IOException {
-    System.setProperty("org.graphstream.ui", "swing");
-    System.setProperty("java.awt.headless", "true");
-    TXTReader txtReader = new TXTReader("graph.txt");
-    TXTReader weightedReader = new TXTReader("weighted.txt");
-    Graph<?, ?> weighted = weightedReader.getWeightedGraph();
-    Graph<?, ?> graph = txtReader.getGraph();
-    new Menu(graph);
-    new Menu(weighted);
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        System.setProperty("org.graphstream.ui", "swing");
+        System.setProperty("java.awt.headless", "true");
+
+        if (args.length == 0) {
+            System.out.println("Erro: Faltou o nome do arquivo de entrada!");
+            return;
+        }
+        String nomeArquivo = args[0];
+
+        System.out.println("Lendo arquivo: " + nomeArquivo);
+        TXTReader meuLeitor = new TXTReader(nomeArquivo);
+        Graph<String, Double> meuGrafo = meuLeitor.getGraph();
+        
+        if (meuGrafo == null) {
+            System.out.println("Nao consegui carregar o grafo.");
+            return;
+        }
+        
+        System.out.println("Iniciando analise de comunidades...");
+        DFS<String, Double> meuAnalisador = new DFS<>();
+        List<List<String>> resultados = meuAnalisador.findConnectedComponents(meuGrafo);
+
+        System.out.println("\n>> Analise Concluida:");
+        System.out.println("Total de comunidades encontradas: " + resultados.size());
+        for (int i = 0; i < resultados.size(); i++) {
+            System.out.println("Grupo " + (i + 1) + ": " + resultados.get(i));
+        }
+        
+        System.out.println("\nGerando imagem do grafo...");
+        GraphView visualizador = new GraphView(meuGrafo);
+        visualizador.generatePNG("visualizacao_grafo.png");
+        System.out.println("Imagem 'visualizacao_grafo.png' foi salva.");
+
+        System.out.println("\nExecucao terminada.");
+    }
 }
--- src/main/java/org/example/algorythms/DFS.java
+++ src/main/java/org/example/algorythms/DFS.java
@@ -2,26 +2,44 @@ package org.example.algorythms;
 
 import org.example.graph.interfaces.Graph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DFS<V, U> {
-    public DFS(Graph<V, U> graph){
+
+    public DFS() {
+    }
+
+    public List<List<V>> findConnectedComponents(Graph<V, U> graph) {
+        List<List<V>> allComponents = new ArrayList<>();
         HashMap<V, Boolean> visitedVertexes = new HashMap<>();
-        for (V vertex : graph.vertexSet()){
+        
+        for (V vertex : graph.vertexSet()) {
             visitedVertexes.put(vertex, Boolean.FALSE);
         }
-        for(V vertex : visitedVertexes.keySet()){
+
+        for (V vertex : visitedVertexes.keySet()) {
             if (!visitedVertexes.get(vertex)) {
-                dfs(vertex, visitedVertexes, graph);
+                List<V> currentComponent = new ArrayList<>();
+        
+                dfs(vertex, visitedVertexes, graph, currentComponent);
+                allComponents.add(currentComponent);
             }
         }
+        
+        return allComponents;
     }
-    private void dfs(V vertex, HashMap<V, Boolean> visitedVertices, Graph<V, U> graph){
+
+    private void dfs(V vertex, HashMap<V, Boolean> visitedVertices, Graph<V, U> graph, List<V> currentComponent) {
         visitedVertices.put(vertex, Boolean.TRUE);
-        System.out.println("visitando: "+vertex);
-        for(V neighbour : graph.getNeightbours(vertex)){
-            if(!visitedVertices.get(neighbour)){
-                dfs(neighbour, visitedVertices, graph);
+        currentComponent.add(vertex);
+
+        if (graph.getNeightbours(vertex) != null) {
+            for (V neighbour : graph.getNeightbours(vertex)) {
+                if (!visitedVertices.get(neighbour)) {
+                    dfs(neighbour, visitedVertices, graph, currentComponent);
+                }
             }
         }
     }
